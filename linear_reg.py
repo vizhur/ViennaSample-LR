faaf30e279671f914e21543d4772979d60205517
Hai Ning <haining@microsoft.com>
2017-06-15 09:57:33 -0400

    fixed

--- linear_reg.py
+++ linear_reg.py
@@ -1,8 +1,11 @@
+# Linear regression on one-dimensional data using a closed form solution.
 # Please make sure matplotlib is included in the conda_dependencies.yml file.
-# Linear regression on one-dimensional data.
 
 import numpy as np
-#import matplotlib.pyplot as plot
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plot
+fig = plot.figure()
 
 # load data
 X = []
@@ -15,9 +18,6 @@ for line in open('data.csv'):
 # turn them into numpy arrays so we can apply matrix operations
 X = np.array(X)
 Y = np.array(Y)
-
-# linear regression closed form solution
-# you can reach this by taking derivatives on the cost function and do simple arithmatics
 
 # this is the common denominator
 denominator = X.dot(X) - X.mean() * X.sum()
@@ -32,16 +32,13 @@ b = (Y.mean() * X.dot(X) - X.mean() * X.dot(Y)) / denominator
 Yhat = a * X + b
 print ("Coefficient: {0}, intercept: {1}".format(a, b))
 
-# Plot the data and the fitted line. Note this only works if you are running against local context.
-#try:
-#    plot.scatter(X, Y)
-#    plot.plot(X, Yhat, color='magenta')
-#    plot.show()
-#except:
-#    print('No display available')
+# Plot the data and the fitted line, then save it into a png file in the output directory.
+ax = fig.gca()
+ax.scatter(X, Y)
+ax.plot(X, Yhat, color='magenta')
+fig.savefig('./outputs/lin.png')
 
-# compute r-squared
-
+### compute r-squared ###
 # residual error of the prediction
 d1 = Y - Yhat
 # intrinsic error to mean
@@ -51,5 +48,4 @@ d2 = Y - Y.mean()
 # if r2 is 0 (i.e., d1 is the same as d2), this is a useless model as it is just the same as predicting mean.
 # if r2 is less than 0 (i.e., d1 is larger that d2), you are doing worse than predicting mean!!
 r2 = 1 - d1.dot(d1) / d2.dot(d2)
-
 print ("R-squared: {}.".format(r2))
